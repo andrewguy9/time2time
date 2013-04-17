60643d13505a13e2af123a3fc376b642607bbe43
Andrew Thomson <andrewguy9@Andrews-MacBook-Pro.local>
2013-04-16 19:15:32 -0700

    move split_line into lib.
    Now called split_time_str.

--- time2time/__init__.py
+++ time2time/__init__.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python
 
+def split_time_str(expr, line):
+    """Takes a line of input and a time expression.
+    Splits the line into a timestamp and the rest of the line."""
+    match = expr.match(line)
+    if match:
+        date = match.group()
+        rest = line[match.end():]
+        return (date,rest)
+    else:
+        return (None,line)
+
