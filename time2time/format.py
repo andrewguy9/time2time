4cddb398797639e313e23abc134c05629aeeabf5
Andrew Thomson <andrewguy9@Andrews-MacBook-Pro.local>
2013-07-10 10:21:37 -0700

    Change perseids time format.
    No longer support numeric timezones.

--- time2time/format.py
+++ time2time/format.py
@@ -2,7 +2,7 @@
 system_fmt = "%b %d %H:%M:%S"
 
 # perseids log 68.201.44.180 [19/Feb/2013:17:42:44 +0000]
-perseids_fmt = "[%d/%b/%Y:%H:%M:%S %z]"
+perseids_fmt = "[%d/%b/%Y:%H:%M:%S +0000]"
 
 # HTTP Format Thu, 23 May 2013 21:21:18 GMT
 http_fmt = "%a, %d %b %G %H:%M:%S GMT"
