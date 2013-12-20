161c724527cb48925f40f391c7272f09c0e94794
Andrew Thomson <athomsonguy@gmail.com>
2013-12-20 09:09:46 -0800

    Throw ValueError on no match in line.

--- time2time/__init__.py
+++ time2time/__init__.py
@@ -14,13 +14,13 @@ def get_time_match(format_str, line):
 def get_time_str(format_str, line):
     """Takes a line and finds a timestamp which matches the time expr"""
     match = get_time_match(format_str, line)
-    date = match.group()
-    return date
+    if match:
+        date = match.group()
+        return date
+    else:
+        raise ValueError("%s does not occur in '%s'" % (format_str, line))
 
 def get_time(format_str, line):
     """Takes a line and finds a time which matches the expr"""
     date = get_time_str(format_str, line)
-    if date is None:
-        return None
-    else:
-        return strptime(date, format_str)
+    return strptime(date, format_str)
